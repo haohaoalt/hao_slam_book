0d0642580ec7d1d20dcb6f0206c6747c182ce14d
张好好 <73096765+haohaoalt@users.noreply.github.com>
2022-10-12 20:55:31 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -43,5 +43,9 @@ def setup(app):
     app.add_config_value('recommonmark_config', {
             'url_resolver': lambda url: github_doc_root + url,
             'auto_toc_tree_section': 'Contents',
+            'enable_math': True,
+            'enable_inline_math': True,
+            'enable_eval_rst': True,
+            'enable_auto_doc_ref': True,
             }, True)
     app.add_transform(AutoStructify)
