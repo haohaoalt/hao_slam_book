0f28c8a91e4bf4bddaef46c6f84a32ba89d2321b
张好好 <73096765+haohaoalt@users.noreply.github.com>
2022-10-12 20:18:57 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,7 @@ release = 'v1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+pip install recommonmark 
 from recommonmark.parser import CommonMarkParser
 source_parsers = {
     '.md': CommonMarkParser,
